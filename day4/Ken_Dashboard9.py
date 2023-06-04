9b4c5f5b93255f6f4cd625e94271311d49c67768
Onsil-TheGreenHouse <zaxs1029za@gmail.com>
2023-06-05 03:24:46 +0900

    update csv path
    - github 배포 시, 에러나서 변경해서 다시 시도

--- day4/Ken_Dashboard9.py
+++ day4/Ken_Dashboard9.py
@@ -37,7 +37,7 @@ def audience_simple(country):
 # load data
 @st.cache_data
 def load_data():
-    df_agg = pd.read_csv('data/Aggregated_Metrics_By_Video.csv')
+    df_agg = pd.read_csv('./data/Aggregated_Metrics_By_Video.csv')
     df_agg = df_agg.iloc[1:, :]
     df_agg.columns = [
         'Video', 'Video title', 'Video publish time', 'Comments added', 'Shares', 'Dislikes', 'Likes',
@@ -62,10 +62,10 @@ def load_data():
     df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
     df_agg.sort_values('Video publish time', ascending=False, inplace=True)
 
-    df_agg_sub = pd.read_csv('data/Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
+    df_agg_sub = pd.read_csv('./data/Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
 
-    df_comments = pd.read_csv('data/Aggregated_Metrics_By_Video.csv')
-    df_time = pd.read_csv('data/Video_Performance_Over_Time.csv')
+    df_comments = pd.read_csv('./data/Aggregated_Metrics_By_Video.csv')
+    df_time = pd.read_csv('./data/Video_Performance_Over_Time.csv')
     df_time['Date'] = pd.to_datetime(df_time['Date'].apply(lambda x: x.replace('Sept', 'Sep')))
 
     return df_agg, df_agg_sub, df_comments, df_time
